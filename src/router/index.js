ce781ccc689d6abdc72c5ffa10f6c2f4cca1467a
tangshuimei <995170180@qq.com>
2020-05-04 07:48:39 +0800

    新增父子组件生命周期frag补充

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '../page/index'
 import Parent from '../components/Parent/Parent'
+import LifeCycle from '../components/LifeCycle/Parent'
 
 Vue.use(Router)
 
@@ -20,6 +21,11 @@ let routes = [
         path: '/parent', // 组件通信演示
         name: 'parent',
         component: Parent
+    },
+    {
+        path: '/lifeCycle', // 父子组件生命周期演示
+        name: 'lifeCycle',
+        component: LifeCycle
     }
 ]
 
